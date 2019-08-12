a8fa9c6d31a00abfb76c1c4717b18b041f52b4c5
christian <nocerino.christian.cn@gmail.com>
2019-08-12 13:51:20 +0200

    Rimossa classe db  e connessione eseguite  sul main activity

--- app/src/main/java/com/example/greencity/MainActivity.kt
+++ app/src/main/java/com/example/greencity/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.greencity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.util.Log
 import android.widget.TextView
+import com.mongodb.stitch.android.core.Stitch
+import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val client = Stitch.initializeDefaultAppClient("greencity-pfvds")
+        client.auth.loginWithCredential(AnonymousCredential()).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(
+                    "myApp", String.format(
+                        "logged in as user %s with provider %s",
+                        task.result.id,
+                        task.result.loggedInProviderType
+                    )
+                )
+            } else {
+                Log.e("myApp", "failed to log in", task.exception)
+            }
+        }
         setContentView(R.layout.activity_main)
         textSignIn = findViewById(R.id.sign_in)
-        textSignIn?.setOnClickListener(View.OnClickListener {
+        textSignIn?.setOnClickListener {
             val iSignIn = Intent(this, SignIn::class.java)
             startActivity(iSignIn)
-
-            val conDB: ConnectionDB? = null
-            conDB?.conncetionDB()
-        })
+        }
 
     }
 }
