09c9c652c4e94b6bc93713a33f18bc784890e3ce
christian <nocerino.christian.cn@gmail.com>
2019-08-13 15:49:49 +0200

    Rimossa classe db  e connessione eseguite  sul main activity

--- app/src/main/java/com/example/greencity/SignIn.kt
+++ app/src/main/java/com/example/greencity/SignIn.kt
@@ -1,20 +1,13 @@
 package com.example.greencity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
-import com.google.android.gms.tasks.OnCompleteListener
 import com.mongodb.stitch.android.core.Stitch
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult
-import org.bson.Document
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient
-import com.mongodb.stitch.android.core.StitchAppClient
-
-
-
-
+import org.bson.Document
+import java.util.*
 
 
 class SignIn : AppCompatActivity() {
@@ -22,31 +15,14 @@ class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        val client = Stitch.initializeDefaultAppClient("greencity-pfvds")
-        var mongoClient = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
-        var itemsCollection = mongoClient.getDatabase("greencitydb").getCollection("user")
-        // get reference to button
-        val btn_Register = findViewById(R.id.register_btn) as Button;
-        // set on-click listener
-        btn_Register.setOnClickListener {
-            val doc1 = Document()
-                .append("user", "basketball")
+        val client = Stitch.getDefaultAppClient()
+        val mongoClient = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
+        val itemsCollection = mongoClient.getDatabase("GreenCity").getCollection("User")
+        itemsCollection.sync().find()
+        val register: Button = findViewById(R.id.signin_btn)
 
-            val insertTask = itemsCollection.insertOne(doc1)
-            insertTask.addOnCompleteListener(OnCompleteListener<RemoteInsertOneResult> { task ->
-                if (task.isSuccessful) {
-                    Log.d(
-                        "app", String.format(
-                            "successfully inserted item with id %s",
-                            task.result.insertedId
-                        )
-                    )
-                } else {
-                    Log.e("app", "failed to insert document with: ", task.exception)
-                }
-            })
+        register.setOnClickListener {
         }
     }
-
 
 }
