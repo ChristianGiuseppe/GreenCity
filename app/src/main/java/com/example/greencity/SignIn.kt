cd635cdd53c09becaaa5203c5f49b096ed3b743e
chris <nocerino.christian.cn@gmail.com>
2019-08-16 16:59:38 +0200

    diviso tutti i controlli in funzioni

--- app/src/main/java/com/example/greencity/SignIn.kt
+++ app/src/main/java/com/example/greencity/SignIn.kt
@@ -1,26 +1,24 @@
 package com.example.greencity
 
-import android.database.Cursor
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import com.mongodb.stitch.android.core.Stitch
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient
-import org.bson.Document
-import com.mongodb.client.model.Projections
-import com.mongodb.BasicDBObject
-import com.mongodb.client.model.Accumulators.first
 import com.mongodb.client.model.Filters.eq
-import com.mongodb.client.model.Accumulators.first
-
-
+import com.mongodb.stitch.android.core.StitchAppClient
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection
+import org.bson.Document
 
 
 class SignIn : AppCompatActivity() {
+    private var nameSignIn: EditText? = null
+    private var surnameSignIn: EditText? = null
+    private var emailSignIn: EditText? = null
+    private var passwordSignIn: EditText? = null
+
+    private var isValid = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +29,16 @@ class SignIn : AppCompatActivity() {
         val collection = ConnectionDBUtil.getDB();
         //end setup
 
-        val nameSignIn: TextView = findViewById(R.id.signin_nome) as EditText
-        val surnameSignIn = findViewById(R.id.signin_cognome) as EditText;
-        val emailSignIn = findViewById(R.id.signin_email) as EditText;
-        val passwordSignIn = findViewById(R.id.signin_password) as EditText;
-        val document = Document()
-        val users = mutableListOf<Document>()
+        this.nameSignIn = findViewById<EditText>(R.id.signin_nome)
+        this.surnameSignIn = findViewById<EditText>(R.id.signin_cognome)
+        this.emailSignIn = findViewById<EditText>(R.id.signin_email)
+        this.passwordSignIn = findViewById<EditText>(R.id.signin_password)
 
+        val users = mutableListOf<Document>()
+        var cursor = collection.find(eq("nome", "luca"))
+
+
+        Toast.makeText(this, cursor.toString(), Toast.LENGTH_LONG).show()
 
         val register: Button = findViewById(R.id.signin_btn)
 
@@ -50,68 +51,26 @@ class SignIn : AppCompatActivity() {
                 .addOnSuccessListener {
                     users.map {
                         users.forEach {
-                            it.getString("email") // Extract only the 'email' field
-                            Log.i("LUCA2",it.getString("email").toString())
-                            if(emailSignIn.text.toString().trim() == it.getString("email")){
-                                isValid = false;
-                                Log.i("EMAIL ESISTENTE",emailSignIn.text.toString().trim())
-                                Toast.makeText(this,"Utente gia registrato con questa email",Toast.LENGTH_LONG).show()
-                            }
+                            // Extract only the 'email' field
+                            var emailDB = it.getString("email")
+                            Log.i("LUCA2", it.getString("email").toString())
+                            checkExistEmail(emailDB)
                         }
                         // of each document
                     }
                     // More code here
                 }
-            if (nameSignIn.text.toString().trim().length == 0) {
-                nameSignIn.setError("Nome Obbligatorio");
-                isValid = false;
-            } else {
-                nameSignIn.setError(null);
-            }
-            if (surnameSignIn.text.toString().trim().length == 0) {
-                surnameSignIn.setError("Cognome Obbligatorio");
-                isValid = false;
-            } else {
-                surnameSignIn.setError(null);
-            }
-            if (emailSignIn.text.toString().trim().length == 0) {
-                emailSignIn.setError("Email Obbligatorio");
-                isValid = false;
-            } else {
-               if(isEmailValid(emailSignIn.text.toString())){
-                    Toast.makeText(this,"Valid Email",Toast.LENGTH_LONG).show()
-                    emailSignIn.setError(null);
-                }else{
-                    isValid = false;
-                    emailSignIn.setError("Inserire una mail valida");
-                }
-            }
-            if (passwordSignIn.text.toString().trim().length == 0) {
-                passwordSignIn.setError("Password Obbligatorio");
-                isValid = false;
-            } else {
-                if(isValidPassword(passwordSignIn.text.toString())){
-                    Toast.makeText(this,"Valid Password",Toast.LENGTH_LONG).show()
-                    passwordSignIn.setError(null);
-                }else{
-                    isValid = false;
-                    passwordSignIn.setError("La password deve contenere 8 caratteri tra cui maiuscolo, minuscolo,numeri e caratteri speciali");
-                }
-            }
+            checkName()
+            checkSurname()
+            checkEmail()
+            checkPassword()
             if (isValid) {
-                document["nome"] = nameSignIn.text.toString();
-                document["cognome"] = surnameSignIn.text.toString();
-                document["email"] = emailSignIn.text.toString();
-                document["password"] = passwordSignIn.text.toString();
-                document["user_id"] = client.auth.user!!.id
-                collection.insertOne(document).addOnSuccessListener {
-                Log.d("STITCH", "inserito")
-                }
+                sendUser(client, collection)
             }
         }
     }
 
-    fun isValidPassword(password: String?) : Boolean {
+    fun isValidPassword(password: String?): Boolean {
         password?.let {
             val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"
             val passwordMatcher = Regex(passwordPattern)
@@ -123,5 +82,68 @@ class SignIn : AppCompatActivity() {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
+    private fun checkName() {
+        if (nameSignIn?.text.toString().trim().isEmpty()) {
+            nameSignIn?.setError("Nome Obbligatorio");
+            isValid = false
+        } else {
+            nameSignIn?.setError(null)
+        }
+    }
 
+    private fun checkSurname() {
+        if (surnameSignIn?.text.toString().trim().isEmpty()) {
+            surnameSignIn?.setError("Cognome Obbligatorio")
+            isValid = false
+        } else {
+            surnameSignIn?.setError(null)
+        }
+    }
+
+    private fun checkEmail() {
+        if (emailSignIn?.text.toString().trim().length == 0) {
+            emailSignIn?.setError("Email Obbligatorio")
+            isValid = false
+        } else {
+            if (isEmailValid(emailSignIn?.text.toString())) {
+                Toast.makeText(this, "Valid Email", Toast.LENGTH_LONG).show()
+                emailSignIn?.setError(null)
+            } else {
+                isValid = false
+                emailSignIn?.setError("Inserire una mail valida")
+            }
+        }
+    }
+
+    private fun checkExistEmail(it: String) {
+        if (emailSignIn?.text.toString().trim().equals(it)) {
+            isValid = false;
+            Log.i("EMAIL ESISTENTE", emailSignIn?.text.toString().trim())
+            Toast.makeText(this, "Utente gia registrato con questa email", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPassword() {
+        if (passwordSignIn?.text.toString().trim().length == 0) {
+            passwordSignIn?.setError("Password Obbligatorio");
+            isValid = false
+        } else {
+            if (!isValidPassword(passwordSignIn?.text.toString())) {
+                isValid = false
+                passwordSignIn?.setError("La password deve contenere 8 caratteri tra cui maiuscolo, minuscolo,numeri e caratteri speciali")
+            }
+        }
+    }
+
+    private fun sendUser(client: StitchAppClient, collection: RemoteMongoCollection<Document>) {
+        val document = Document()
+        document["nome"] = nameSignIn?.text.toString()
+        document["cognome"] = surnameSignIn?.text.toString()
+        document["email"] = emailSignIn?.text.toString()
+        document["password"] = passwordSignIn?.text.toString()
+        document["user_id"] = client.auth.user!!.id
+        collection.insertOne(document).addOnSuccessListener {
+            Log.d("STITCH", "inserito")
+        }
+    }
 }
