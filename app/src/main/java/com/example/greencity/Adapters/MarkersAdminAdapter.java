28d641ccac7bdb958b5f087a5af758c38502b2cc
lucamoggio6@gmail.com <lucamoggio1995>
2020-01-09 07:48:57 +0100

    Lista reportAdmin , aggiunto onclick cambio stato Done Report

--- app/src/main/java/com/example/greencity/Adapters/MarkersAdminAdapter.java
+++ app/src/main/java/com/example/greencity/Adapters/MarkersAdminAdapter.java
@@ -6,20 +6,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.greencity.DBFirebase;
 import com.example.greencity.R;
 import com.example.greencity.pojo.Markers;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MarkersAdminAdapter extends ArrayAdapter<Markers> {
 
     private List<Markers> markersList;
+
 
     public MarkersAdminAdapter(@NonNull Context context, int resource, @NonNull List<Markers> objects) {
         super(context, resource, objects);
@@ -38,8 +49,44 @@ public class MarkersAdminAdapter extends ArrayAdapter<Markers> {
         ImageButton imageButtonReject = (ImageButton) convertView.findViewById(R.id.btn_reject);
         titleReport.setText(markersList.get(position).getTitolo());
         dateReport.setText(markersList.get(position).getDataOra());
+        final String[] idkeyUserMarkerClick = {markersList.get(position).getKeyUser()};
 
         imageButtonDone.setOnClickListener(view -> {
+            DatabaseReference fromPath = FirebaseDatabase.getInstance().getReference("lista_wait");
+            DatabaseReference toPath = FirebaseDatabase.getInstance().getReference("lista_done");
+
+            //copyRecord(fromPath,toPath,position);
+            ValueEventListener reportListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // obtain list of java objects from the datasnapshot
+                    for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                        Markers markers = postSnapshot.getValue(Markers.class);
+                        if(idkeyUserMarkerClick[0] != "" ){
+                            idkeyUserMarkerClick[0] = "";
+                        markers.setStato("DONE");
+                        String keyMark = postSnapshot.getKey();
+                        fromPath.child(keyMark).setValue(null);
+                        toPath.child(keyMark).setValue(markers);
+                        
+                        break;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+            DBFirebase.getDbFirebase().getDatabaseReference().child("lista_wait").orderByChild("keyUser").equalTo(idkeyUserMarkerClick[0].toString())
+                    .addValueEventListener(reportListener);
+
+
+
+
+
+
 
         });
 
@@ -48,4 +95,47 @@ public class MarkersAdminAdapter extends ArrayAdapter<Markers> {
         });
         return convertView;
     }
+
+    private void copyRecord(DatabaseReference fromPath, final DatabaseReference toPath,Integer i) {
+        fromPath.addListenerForSingleValueEvent(new ValueEventListener()  {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                toPath.setValue(dataSnapshot.getValue().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String idkeyUserMarkerClick = markersList.get(i).getKeyUser();
+                        ValueEventListener reportListener = new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                // obtain list of java objects from the datasnapshot
+                                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                                    Markers markers = postSnapshot.getValue(Markers.class);
+                                    markers.setStato("DONE");
+                                    String keyMark = postSnapshot.getKey();
+                                    /*fromPath.child(keyMark).setValue("xxx");
+                                    toPath.child(keyMark).setValue(markers);*/
+                                    break;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        };
+                        DBFirebase.getDbFirebase().getDatabaseReference().child("lista_wait").orderByChild("keyUser").equalTo(idkeyUserMarkerClick.toString())
+                                .addValueEventListener(reportListener);
+
+
+
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+    }
+
+
+
 }
--- app/src/main/java/com/example/greencity/activity/SplashGreenCity.kt
+++ app/src/main/java/com/example/greencity/activity/SplashGreenCity.kt
@@ -1,6 +1,4 @@
 package com.example.greencity.activity
-
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +8,7 @@ import com.example.greencity.pojo.Markers
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import java.lang.Exception
 import android.content.Context
-import java.io.File
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.util.Log
-import com.google.firebase.database.FirebaseDatabase
 
 
 class SplashGreenCity : AppCompatActivity() {
@@ -25,34 +16,92 @@ class SplashGreenCity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.greencity.R.layout.activity_splash_green_city)
-            val iLogin = Intent(this, Navbar::class.java)
-            var listMarkers: ArrayList<Markers> = ArrayList()
-            var list_done: ArrayList<Markers> = ArrayList()
-            var list_reject: ArrayList<Markers> = ArrayList()
-            var list_wait: ArrayList<Markers> = ArrayList()
-            var idKey = InformazioniGenerali.getInformazioniGenerali().idUs
-            val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
-            val idUsSP = sharedPreferences?.getString("IDUSER","").toString()
+        val iLogin = Intent(this, Navbar::class.java)
+        var list_done: ArrayList<Markers> = ArrayList()
+        var list_reject: ArrayList<Markers> = ArrayList()
+        var list_wait: ArrayList<Markers> = ArrayList()
+        var idKey = InformazioniGenerali.getInformazioniGenerali().idUs
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val idUsSP = sharedPreferences?.getString("IDUSER","").toString()
 
 
-            // CICLO LISTA WAITING
-            val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
+
+        val reportsListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
+                for (reportSnapshot in dataSnapshot.getChildren()) {
+                    var mark = reportSnapshot.getValue()!!
+                    if(mark.equals("true")){
+                        adminListReportWait(list_wait)
+                        adminListReportDone(list_done)
+                        adminListReportReject(list_reject)
+                        break
+                    }
+                    else{
+                        userListReportWait(list_wait)
+                        userListReportDone(list_done)
+                        userListReportReject(list_reject)
+                        break
+                    }
+                }
+                startActivity(iLogin)
+                finish()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
+
+            }
+        }
+        if( idKey != null){
+
+            DBFirebase.getDbFirebase().databaseReference.child("users").child(idKey)
+                .addValueEventListener(reportsListener)
+            DBFirebase.getDbFirebase().databaseReference.removeEventListener(reportsListener)
+
+        }
+        else{
+
+            DBFirebase.getDbFirebase().databaseReference.child("users").child(idUsSP)
+                .addValueEventListener(reportsListener)
+            DBFirebase.getDbFirebase().databaseReference.removeEventListener(reportsListener)
+
+        }
+
+
+
+    }
+
+
+
+    private fun userListReportWait(list_wait :  ArrayList<Markers>){
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val idUsSP = sharedPreferences?.getString("IDUSER","").toString()
+
+        // CICLO LISTA WAITING
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
                 for (reportSnapshot in dataSnapshot.getChildren()) {
                     var rep_wait: Markers = reportSnapshot.getValue(Markers::class.java)!!
                     list_wait.add(rep_wait)
                     InformazioniGenerali.getInformazioniGenerali().listaWait = list_wait
-                    }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
-
                 }
             }
-            DBFirebase.getDbFirebase().databaseReference.child("lista_wait").addValueEventListener(reportsListenerWaiting)
+
+            override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
+
+            }
+        }
+
+        DBFirebase.getDbFirebase().databaseReference.child("lista_wait").orderByChild("keyUser").equalTo(idUsSP.toString())
+            .addValueEventListener(reportsListenerWaiting)
+
+    }
+
+    private fun userListReportDone(list_done :  ArrayList<Markers>){
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val idUsSP = sharedPreferences?.getString("IDUSER","").toString()
 
         // CICLO LISTA DONE
-        val reportsListenerDone: ValueEventListener = object : ValueEventListener {
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
                 for (reportSnapshot in dataSnapshot.getChildren()) {
                     var rep_done: Markers = reportSnapshot.getValue(Markers::class.java)!!
@@ -65,11 +114,18 @@ class SplashGreenCity : AppCompatActivity() {
 
             }
         }
-        DBFirebase.getDbFirebase().databaseReference.child("lista_done").addValueEventListener(reportsListenerDone)
 
+        DBFirebase.getDbFirebase().databaseReference.child("lista_done").orderByChild("keyUser").equalTo(idUsSP.toString())
+            .addValueEventListener(reportsListenerWaiting)
+
+    }
+
+    private fun userListReportReject(list_reject :  ArrayList<Markers>){
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val idUsSP = sharedPreferences?.getString("IDUSER","").toString()
 
         // CICLO LISTA REJECT
-        val reportsListenerReject: ValueEventListener = object : ValueEventListener {
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
                 for (reportSnapshot in dataSnapshot.getChildren()) {
                     var rep_reject: Markers = reportSnapshot.getValue(Markers::class.java)!!
@@ -82,52 +138,76 @@ class SplashGreenCity : AppCompatActivity() {
 
             }
         }
-        DBFirebase.getDbFirebase().databaseReference.child("lista_reject").addValueEventListener(reportsListenerReject)
 
-
-
-
-
-            val reportsListener: ValueEventListener = object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
-                    for (reportSnapshot in dataSnapshot.getChildren()) {
-                        var mark: Markers = reportSnapshot.getValue(Markers::class.java)!!
-                        listMarkers.add(mark)
-
-                    }
-                    val informazioniGenerali = InformazioniGenerali.getInformazioniGenerali()
-                    informazioniGenerali.markers = listMarkers
-                    startActivity(iLogin)
-                    finish()
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
-
-                }
-            }
-        if( idKey != null){
-
-            DBFirebase.getDbFirebase().databaseReference.child("users").child(idKey)
-                .child("lista_report").addValueEventListener(reportsListener)
-            DBFirebase.getDbFirebase().databaseReference.removeEventListener(reportsListener)
-
-        }else{
-
-            DBFirebase.getDbFirebase().databaseReference.child("users").child(idUsSP)
-                .child("lista_report").addValueEventListener(reportsListener)
-            DBFirebase.getDbFirebase().databaseReference.removeEventListener(reportsListener)
-
-        }
-
-
-
-
-
+        DBFirebase.getDbFirebase().databaseReference.child("lista_reject").orderByChild("keyUser").equalTo(idUsSP.toString())
+            .addValueEventListener(reportsListenerWaiting)
 
     }
 
 
+    private fun adminListReportWait(list_wait :  ArrayList<Markers>){
+        // CICLO LISTA WAITING
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
+                for (reportSnapshot in dataSnapshot.getChildren()) {
+                    var rep_wait: Markers = reportSnapshot.getValue(Markers::class.java)!!
+                        list_wait.add(rep_wait)
+                        InformazioniGenerali.getInformazioniGenerali().listaWait = list_wait
 
+                }
+            }
 
+            override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
+
+            }
+        }
+
+        DBFirebase.getDbFirebase().databaseReference.child("lista_wait")
+            .addValueEventListener(reportsListenerWaiting)
+
+    }
+
+    private fun adminListReportDone(list_done :  ArrayList<Markers>){
+        // CICLO LISTA DONE
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
+                for (reportSnapshot in dataSnapshot.getChildren()) {
+                    var rep_done: Markers = reportSnapshot.getValue(Markers::class.java)!!
+                    list_done.add(rep_done)
+                    InformazioniGenerali.getInformazioniGenerali().listaDone = list_done
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
+
+            }
+        }
+
+        DBFirebase.getDbFirebase().databaseReference.child("lista_done")
+            .addValueEventListener(reportsListenerWaiting)
+
+    }
+
+    private fun adminListReportReject(list_reject :  ArrayList<Markers>){
+        // CICLO LISTA REJECT
+        val reportsListenerWaiting: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // Get Post object and use the values to update the UI
+                for (reportSnapshot in dataSnapshot.getChildren()) {
+                    var rep_reject: Markers = reportSnapshot.getValue(Markers::class.java)!!
+                    list_reject.add(rep_reject)
+                    InformazioniGenerali.getInformazioniGenerali().listaReject = list_reject
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) { // Getting Post failed, log a message
+
+            }
+        }
+
+        DBFirebase.getDbFirebase().databaseReference.child("lista_reject")
+            .addValueEventListener(reportsListenerWaiting)
+
+    }
 
 }
+
